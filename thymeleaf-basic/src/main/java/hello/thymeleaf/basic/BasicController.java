8c0e47970c801b25b7cfe75be05b1fd8db674a13
phwGithub <yey6689@gmail.com>
2023-08-18 17:06:03 +0900

    리터럴

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -80,6 +80,14 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+
+        model.addAttribute("data", "Spring!");
+
+        return "basic/literal";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
