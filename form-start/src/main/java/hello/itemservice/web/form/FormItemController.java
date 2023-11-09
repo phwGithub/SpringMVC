cdaa74fb3bb76fea0a2b7951ab8121a33ffd3175
phwGithub <yey6689@gmail.com>
2023-11-09 13:00:03 +0900

    셀렉트 박스

--- form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -72,6 +72,7 @@ public class FormItemController {
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
+        log.info("item.deliveryCode={}", item.getDeliveryCode());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
